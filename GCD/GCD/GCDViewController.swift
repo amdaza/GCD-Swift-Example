14e7ce36a40a77e8069b97f436c9f6a631a6dcf2
Alicia Daza <amdazacastillo@gmail.com>
2016-09-06 20:45:47 +0200

    Empty app

---
+++ GCD/GCD/GCDViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GCDViewController.swift
+//  GCD
+//
+//  Created by Home on 6/9/16.
+//  Copyright © 2016 Alicia. All rights reserved.
+//
+
+import UIKit
+
+class GCDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
